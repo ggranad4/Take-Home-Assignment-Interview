343b1766a16306b7e7a31be15bfc231a64539fa3
ggranad4 <Gerardogranados45@yahoo.com>
2025-09-09 15:12:42 -0700

    Found constant player and consistently countering

--- rock-paper-scissors/custom-strategy.js
+++ rock-paper-scissors/custom-strategy.js
@@ -1,10 +1,26 @@
-exports.name = 'custom';
-
+exports.name = "custom";
+const counterMoves = {
+  scissors: "rock",
+  rock: "paper",
+  paper: "scissors",
+};
 const histories = {};
+let constantPlayer = {};
 
 exports.recordShot = (playerId, shot) => {
+  if (!histories[playerId]) {
+    histories[playerId] = new Set();
+    histories[playerId].add(shot);
+  }
+  histories[playerId].add(shot);
+  if (histories[playerId].size > 1) {
+    constantPlayer[playerId] = shot;
+  }
 };
 
 exports.makeShot = (playerId) => {
-    return 'foo';
+  if (constantPlayer[playerId]) {
+    return counterMoves[constantPlayer[playerId]];
+  }
+  return "foo";
 };
